8796688c2419e34588230c59739ded961dbd3640
viswanath4182 <viswanath4182@gmail.com>
2019-10-16 21:00:22 +0200

    First Commit

---
+++ messenger/src/main/java/web/rest/messenger/resources/MessageResource.java
@@ -0,0 +1,37 @@
+package web.rest.messenger.resources;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import web.rest.messenger.model.Message;
+import web.rest.messenger.service.MessageService;
+
+@Path("/messages")
+public class MessageResource {
+	MessageService messageService = new MessageService();
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Message> getMessages() {
+		return messageService.getAllMessages();
+}
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
+		
+	}
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message getMessage(@PathParam("messageId") long id) {
+		return messageService.getMessage(id);
+	}
+}
